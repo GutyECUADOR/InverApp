ad876e9078a230487abd5e49806d948d335d93a0
José L. Gutiérrez <gutiecuador@gmail.com>
2022-09-05 16:53:05 -0500

    avance login with laravel

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -5,8 +5,7 @@ import { Router } from '@angular/router';
 import { UserData } from '../../providers/user-data';
 
 import { UserOptions } from '../../interfaces/user-options';
-
-
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'page-login',
@@ -19,12 +18,21 @@ export class LoginPage {
 
   constructor(
     public userData: UserData,
-    public router: Router
+    public router: Router,
+    public alertCtrl: AlertController
   ) { }
 
-  onLogin(form: NgForm) {
+  async onLogin(form: NgForm) {
     this.submitted = true;
+    
+    const alert = await this.alertCtrl.create({
+      header: 'Login Fallido',
+      message: 'Ups no se pudo ingresar a la aplicación, revise su usuario y/o contraseña.',
+      buttons: ['OK'],
+    });
 
+    await alert.present();
+    return;
     if (form.valid) {
       this.userData.login(this.login.username);
       this.router.navigateByUrl('/app/tabs/schedule');
